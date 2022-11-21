4da2dd89b1aa90f90a149ddfebecf2ed365c8729
NIVESH <108427555+RNK9629@users.noreply.github.com>
2022-11-21 13:09:35 +0530

    Update ECRrepo.groovy

--- vars/ECRrepo.groovy
+++ vars/ECRrepo.groovy
@@ -1,1 +1,5 @@
-
+def ecr(String AWS_ACCOUNT_ID,String AWS_DEFAULT_REGION,String IMAGE_REPO_NAME,String IMAGE_TAG,String REPOSITORY_URI)
+{
+  def command="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}"
+  def command="aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
+}
